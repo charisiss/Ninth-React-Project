995cc287d480ccbb3e1e62bd45cf458f6143281b
xsxsam <54262610+xsxsam@users.noreply.github.com>
2022-09-24 17:14:59 +0300

    Final addings to redux login

--- src/store/index.js
+++ src/store/index.js
@@ -1,45 +1,10 @@
-import { createStore } from "redux";
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
-const initialCounterState = { counter: 0, showCounter: true };
-const initialAuthState = { isLoggedIn: false };
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: initialCounterState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increse(state, action) {
-      state.counter += action.payload;
-    },
-    toggle(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const authSlice = createSlice({
-  name: "auth",
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isLoggedIn = true;
-    },
-    logout(state) {
-      state.isLoggedIn = false;
-    },
-  },
-});
+import counterSlice from "./counterSlice";
+import authSlice from "./authSlice";
 
 const store = configureStore({
-  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+  reducer: { counter: counterSlice, auth: authSlice },
 });
 
-export const authActions = authSlice.actions;
-export const counterActions = counterSlice.actions;
 export default store;
